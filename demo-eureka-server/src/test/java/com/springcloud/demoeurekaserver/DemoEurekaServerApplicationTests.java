a2ef2a0cff98521208e732e7c139970c250e8abe
deliciousSaltFish <496057338@qq.com>
2018-12-07 22:28:01 +0800

    配置eureka server 只有8080端口好用,其他都有问题,好困,明天仔细看看咋回事

--- demo-eureka-server/src/test/java/com/springcloud/demoeurekaserver/DemoEurekaServerApplicationTests.java
+++ demo-eureka-server/src/test/java/com/springcloud/demoeurekaserver/DemoEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.james.demoeurekaserver;
+package com.springcloud.demoeurekaserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoEurekaServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
