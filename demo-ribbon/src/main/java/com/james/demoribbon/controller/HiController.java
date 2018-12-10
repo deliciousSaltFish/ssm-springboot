8acd1c68fb69750c8d6a6eef9f30eba88f6ef53f
deliciousSaltFish <496057338@qq.com>
2018-12-10 17:39:26 +0800

    增加Ribbon 负载均衡
    如何一个工程启动多个实例，请看这篇文章:
    https://blog.csdn.net/forezp/article/details/76408139

---
+++ demo-ribbon/src/main/java/com/james/demoribbon/controller/HiController.java
@@ -0,0 +1,20 @@
+package com.james.demoribbon.controller;
+
+import com.james.demoribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        System.out.println(name);
+        return helloService.hiService( name );
+    }
+
+}
